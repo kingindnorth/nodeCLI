0add1f28d51217793a5f0564ce5ece3599ceb395
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-05 17:22:42 +0530

    v:1.0

--- app.js
+++ app.js
@@ -7,19 +7,63 @@ const db = mongoose.connect(process.env.DB).then(()=>{
 })
 
 const addUser = async(user) =>{
-    await User.create(user)
-    console.info("User added")
+    try{
+        await User.create(user)
+        console.info("User added")
+    }catch(err){
+        console.log(err);
+        throw err
+    }
 }
 
 const searchUser = async(name) =>{
-    //make case insensitive
-    const search = new RegExp(name,"i")
-    const user = await User.find({$or:[{firstName:search},{lastName:search}]})
-    console.info(user)
-    console.info(`${user.length} matches`)
+    try{
+        //make case insensitive
+        const search = new RegExp(name,"i")
+        const user = await User.find({$or:[{firstName:search},{lastName:search}]})
+        console.info(user)
+        console.info(`${user.length} matches`)
+    }catch(err){
+        console.log(err);
+        throw err
+    }
+}
+
+const updateUser = async(_id,user) =>{
+    try{
+        await User.updateOne({_id},user)
+        console.info("user updated")
+    }catch(err){
+        console.log(err);
+        throw err
+        }
+}
+
+const deleteUser = async(_id) =>{
+    try{
+        await User.deleteOne({_id})
+        console.info("user deleted")
+    }catch(err){
+        console.log(err);
+        throw err
+        }
+}
+
+const listUser = async() =>{
+    try{
+        const users = await User.find()
+        console.info(users)
+        console.info(users.length+" users")
+    }catch(err){
+        console.log(err);
+        throw err
+        }
 }
 
 module.exports = {
     addUser,
-    searchUser
+    searchUser,
+    updateUser,
+    deleteUser,
+    listUser
 }
--- commander.js
+++ commander.js
@@ -1,24 +1,74 @@
 const program = require("commander")
+const {prompt} = require("inquirer");
 
 const {
     addUser,
-    searchUser
+    searchUser,
+    updateUser,
+    deleteUser,
+    listUser
 } = require("./app")
+
+const questions = [
+    {
+        type:"input",
+        name:"firstName",
+        message:"user first name"
+    },
+    {
+        type:"input",
+        name:"lastName",
+        message:"user last name"
+    },
+    {
+        type:"input",
+        name:"email",
+        message:"user email"
+    },
+    {
+        type:"input",
+        name:"phone",
+        message:"user phone"
+    }
+]
 
 program.version("1.0.0").description("CLI:command line interface")
 
-program.command("add <firstName> <lastName> <email> <phone>")
+program.command("add")
        .alias("a")
        .description("add a user")
-       .action((firstName,lastName,email,phone)=>{
-            addUser({firstName,lastName,email,phone})
+       .action(async()=>{
+            const ans = await prompt(questions)
+            addUser(ans)
        })
 
 program.command("find <name>")
        .alias("f")
        .description("find a user by name")
-       .action((name)=>{
-            searchUser(name)
+       .action(async(name)=>{
+            await searchUser(name)
        })
+
+program.command("update <id>")
+        .alias("u")
+        .description("update a user by id")
+        .action(async(id)=>{
+            const ans = await prompt(questions)
+            updateUser(id,ans)
+        })    
+        
+program.command("delete <id>")
+        .alias("d")
+        .description("delete an user by id")
+        .action(async(id)=>{
+            await deleteUser(id)
+        })        
+
+program.command("list")
+        .alias("l")
+        .description("list of all users")
+        .action(async()=>{
+            await listUser()
+        })
 
 program.parse(process.argv)
